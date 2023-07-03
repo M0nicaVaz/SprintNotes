bb7a4fe3e0a37b4ee3b528b8b9193bf8404e30b4
Monica Vaz <monica.vaz@eumedicoresidente.com>
2023-07-03 14:21:39 -0300

    feat: login page

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -12,6 +12,9 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  pages: {
+    signIn: '/signIn',
+  },
 };
 
 const handler = NextAuth(authOptions);
